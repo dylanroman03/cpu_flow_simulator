714618a0e2382578061e8b8f03e5b3d86df10b03
Dylan Roman <romand.buitrago@gmail.com>
2026-04-21 09:40:41 -0400

    feat: Add OrderByFifoUseCase implementation and corresponding tests

---
+++ lib/features/cpu_flow/domain/use_cases/order_by_fifo_use_case.dart
@@ -0,0 +1,45 @@
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/entities/process.dart';
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/entities/process_runtime.dart';
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/entities/schedule_slice.dart';
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/entities/simulation_result.dart';
+
+class OrderByFifoUseCase {
+  SimulationResult call(List<Process> processes) {
+    final List<Process> sortedProcess = List<Process>.from(processes)
+      ..sort((a, b) => a.arriveTime.compareTo(b.arriveTime));
+
+    List<ScheduleSlice> slices = [];
+    List<ProcessRuntime> processRuntimes = [];
+    int currentTime = 0;
+
+    for (var process in sortedProcess) {
+      ScheduleSlice slice = ScheduleSlice(
+        processId: process.id,
+        startTime: currentTime,
+        endTime: currentTime + process.cpuTime,
+      );
+
+      ProcessRuntime runtime = ProcessRuntime(
+        processId: process.id,
+        remainingTime: 0,
+        waitingTime: slice.startTime - process.arriveTime,
+        firstStartTime: slice.startTime,
+        finishTime: slice.endTime,
+      );
+
+      slices.add(slice);
+      processRuntimes.add(runtime);
+      currentTime = slice.endTime;
+    }
+
+    final avarageWaitingTime =
+        processRuntimes.map((p) => p.waitingTime).reduce((a, b) => a + b) /
+        processRuntimes.length;
+
+    return SimulationResult(
+      slices: slices,
+      totalTime: currentTime,
+      averageWaitingTime: avarageWaitingTime.toDouble(),
+    );
+  }
+}
---
+++ test/features/cpu_flow/domain/use_cases/order_by_fifo_use_case.test.dart
@@ -0,0 +1,41 @@
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/entities/process.dart';
+import 'package:cpu_flow_simulator/features/cpu_flow/domain/use_cases/order_by_fifo_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('OrderByFifoUseCase', () {
+    test('genera el timeline FIFO esperado con los datos del ejemplo', () {
+      final useCase = OrderByFifoUseCase();
+
+      final processes = <Process>[
+        const Process(id: 'P1', arriveTime: 0, cpuTime: 6),
+        const Process(id: 'P2', arriveTime: 4, cpuTime: 3),
+        const Process(id: 'P3', arriveTime: 6, cpuTime: 4),
+        const Process(id: 'P4', arriveTime: 7, cpuTime: 1),
+      ];
+
+      final result = useCase.call(processes);
+
+      expect(result.slices.length, 4);
+
+      expect(result.slices[0].processId, 'P1');
+      expect(result.slices[0].startTime, 0);
+      expect(result.slices[0].endTime, 6);
+
+      expect(result.slices[1].processId, 'P2');
+      expect(result.slices[1].startTime, 6);
+      expect(result.slices[1].endTime, 9);
+
+      expect(result.slices[2].processId, 'P3');
+      expect(result.slices[2].startTime, 9);
+      expect(result.slices[2].endTime, 13);
+
+      expect(result.slices[3].processId, 'P4');
+      expect(result.slices[3].startTime, 13);
+      expect(result.slices[3].endTime, 14);
+
+      expect(result.totalTime, 14);
+      expect(result.averageWaitingTime, closeTo(2.75, 0.0001));
+    });
+  });
+}
